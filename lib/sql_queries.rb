8fb21bf5b12ca03485db39c3da724d308fed3c2b
Erin Walker <erinb0317@hotmail.com>
2018-03-05 03:04:37 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -43,5 +43,7 @@ WHERE projects.category = 'music';"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-"Write your SQL query Here"
+"SELECT projects.category, SUM(pledges.amount) FROM projects
+JOIN pledge ON pledges.project_id = projects.id
+WHERE projects.category = 'books';"
 end
