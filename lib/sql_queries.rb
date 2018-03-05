c687d86bcf57de7c212551c247d4cc240244531e
Erin Walker <erinb0317@hotmail.com>
2018-03-05 03:07:57 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -21,7 +21,7 @@ GROUP BY users.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT projects.title, SUM(pledges.amounts)-projects.funding_goal FROM projects
+"SELECT projects.title, SUM(pledges.amount)-projects.funding_goal FROM projects
 JOIN pledges
 ON pledges.project_id = projects.id
 GROUP BY projects.id
@@ -29,7 +29,7 @@ HAVING SUM(pledges.amount)-projects.funding_goal >=0;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"SELECT users.names, SUM(pledges.amounts) FROM users
+"SELECT users.names, SUM(pledges.amount) FROM users
 JOIN pledges
 ON pledges.user_id = users.id
 GROUP BY users.name
@@ -44,6 +44,6 @@ end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
 "SELECT projects.category, SUM(pledges.amount) FROM projects
-JOIN pledge ON pledges.project_id = projects.id
+JOIN pledges ON pledges.project_id = projects.id
 WHERE projects.category = 'books';"
 end
